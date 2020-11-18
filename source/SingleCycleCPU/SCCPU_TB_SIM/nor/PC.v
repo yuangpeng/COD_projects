673e5a0b4f9cfb2149994f2c09a46e64a2020dc0
Bronkape <yuangpeng.cn@gmail.com>
2020-11-18 20:32:23 +0800

    finish nor

---
+++ source/SingleCycleCPU/SCCPU_TB_SIM/nor/PC.v
@@ -0,0 +1,16 @@
+module PC(
+input				clk,
+input				rst,
+input		[31:0]	NPC,
+
+output	reg	[31:0]	PC
+);
+
+	always@(posedge clk, posedge rst) begin
+		if (rst) 
+			PC <= 32'h0000_0000;
+		else
+			PC <= NPC;
+	end
+
+endmodule
